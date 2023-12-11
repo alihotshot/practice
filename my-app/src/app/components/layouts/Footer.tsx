a8d44bc9f6041f8ae92cd19ce5f8eed49aa91d00
alihaidergithub <vergefuture@gmail.com>
2023-12-11 20:43:57 +0500

    import Logo from "/public/next.svg"
    import Logo from "/public/next.svg"

--- my-app/src/app/components/layouts/Footer.tsx
+++ my-app/src/app/components/layouts/Footer.tsx
@@ -1,12 +1,24 @@
+import Image from "next/image"
+import Logo from "/public/Footer-Logo.png"
 
 const Footer = () => {
     return (
         <footer>
-            <div className=" bg-blue-950 text-white text-sm text-center py-2">
-                © Tile Mountain, Inc. All right reserved.
-            </div>
-            <div className=" bg-blue-950 text-white text-sm text-center py-2">
-                © Tile Mountain, Inc. All right reserved.
+            <div>
+                <div className=" bg-blue-950 text-white text-sm text-center py-16">
+                    <div className="flex justify-center">
+                        <Image src={Logo} alt={"Footer Logo"} width={120} height={120}></Image>
+                    </div>
+                    <ul className="flex space-x-6 justify-center text-lg mt-10">
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Documentation</li>
+                        <li>Contact</li>
+                    </ul>
+                    <p className=" mt-6">
+                        © Tile Mountain, Inc. All right reserved.
+                    </p>
+                </div>
             </div>
         </footer>
     )
