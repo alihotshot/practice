6b6de74010dd354d88932e061cf794d18102e5b2
alihaidergithub <vergefuture@gmail.com>
2023-12-13 20:54:56 +0500

    12 13
    12 13

--- my-app/src/app/components/widgets/Boxes.tsx
+++ my-app/src/app/components/widgets/Boxes.tsx
@@ -4,23 +4,36 @@ const coreBoxes: { header: string, description: string, number: number }[] = [
         header: 'Quarter I',
         description: 'CS-101: Object-Oriented Programming using Typescript.',
         number: 1
+    },
+    {
+        header: 'Quarter II',
+        description: 'W2-201: Developing Planet-Scale Web 2.0 Serverless Cloud Cloud Apps and APIs using Next.js 13 and Cloud Development Kit (CDK) for Terraform',
+        number: 2
+    },
+    {
+        header: 'Quarter III',
+        description: '$-101: Dollar Making Bootcamp - Full-Stack Template and API Product Development',
+        number: 3
     }
 ]
 
 const Boxes = () => {
     return (
-        <div>
+        <div className="flex max-w-screen-xl mx-auto py-6 space-x-4">
             {
-                coreBoxes.map((item) => {
+                coreBoxes.map((item, i) => {
                     return (
-                        <div className="max-w-screen-xl mx-auto relative">
-                            <h4 className=" font-semibold text-lg">Quarter I</h4>
-                            <p className=" text-slate-500 ">CS-101: Object-Oriented Programming using Typescript.</p>
-                            <span className="text-8xl font-sans font-bold opacity-10 absolute top-4 ">1</span>
+                        <div className="border rounded-lg w-4/12 px-8 py-12 relative">
+                            <h4 className=" font-semibold text-lg">{item.header}</h4>
+                            <p className=" text-slate-600 mt-2">{item.description}</p>
+                            <span className="text-9xl right-10 font-bold opacity-10 absolute top-4">{i + 1}</span>
                         </div>
                     )
                 })
             }
+            <br />
+            <br />
+            <br />
             <br />
             <br />
             <br />
