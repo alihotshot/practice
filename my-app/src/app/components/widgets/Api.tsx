812a22ed50b94a509c72d436ad89bf8dd34daaf4
Ali <vergefuture@gmail.com>
2023-12-23 16:58:22 +0500

    23
    c23

--- my-app/src/app/components/widgets/Api.tsx
+++ my-app/src/app/components/widgets/Api.tsx
@@ -1,24 +1,8 @@
-import { log } from "console"
 
-async function Api() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        headers: {
-            Authorization: `Bearer 511f633bf09d9b215b26c1b6d5dcbb38169041b84fa458ae24f6ead71e07676b`
-        },
-        body: JSON.stringify({
-            "bookId": 3,
-            "customerName": "Ali Haider"
-        })
-    })
-
-    const data = response.json()
-
+const BlogApi = () => {
     return (
-        <div>
-            {data.i}
-        </div>
+        <div></div>
     )
 }
 
-export default Api
+export default BlogApi
