14090e8242c35d7e4ec4a30a6cb476a8fc83df3b
Ali <vergefuture@gmail.com>
2023-12-25 09:08:47 +0500

    2512
    2512

---
+++ my-app/sanity/blogs.ts
@@ -0,0 +1,18 @@
+
+export const blogs = {
+    name: "blog",
+    type: "document",
+    title: "Blogs",
+    fields: [
+        {
+            name: "title",
+            title: "Blog Title",
+            type: "string"
+        },
+        {
+            name: "title",
+            title: "Blog Title",
+            type: "string"
+        }, 
+    ]
+}
---
+++ my-app/sanity/products.ts
@@ -0,0 +1,23 @@
+
+export const products = {
+    name: "product",
+    type: "document",
+    title: "Products",
+    fields: [
+        {
+            name: "title",
+            title: "Product Title",
+            type:"string"
+        },
+        {
+            name: "description",
+            title: "Description",
+            type:"string"
+        },
+        {
+            name: "image",
+            title: "Upload Image",
+            type:"image"
+        }, 
+    ]
+}
