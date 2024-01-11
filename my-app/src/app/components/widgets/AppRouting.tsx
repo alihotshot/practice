a1706925c40196ebe445681d93a83bfb8140252f
alihaidergithub <vergefuture@gmail.com>
2024-01-11 13:30:22 +0500

    11
    11

--- my-app/src/app/components/widgets/AppRouting.tsx
+++ my-app/src/app/components/widgets/AppRouting.tsx
@@ -1,8 +1,7 @@
-// Dynamic Routing : We also call it "App Routing" 
+// // Dynamic Routing : We also call it "App Routing" 
 'use client'
-import { useState } from "react"
-import Button from "../shared/Button"
 import axios from "axios"
+import { useState } from "react"
 
 const AppRouting = () => {
     const [users, setusers] = useState([])
@@ -12,18 +11,21 @@ const AppRouting = () => {
     }
 
     return (
-        <div className="">
-            <div className="">
+        <div className="flex space-x-4 mb-5 items-center justify-center">
+            <div>
                 <button onClick={getUsers} className="text-lg font-medium rounded-full bg-teal-900 text-white px-7 py-3 shadow-xl hover:scale-105">Get Data</button>
             </div>
-            <div className="bg-slate-300 text-xl p-2 my-5 max-w-xs">
-                Data is: {
-                    users.map((e) => {
-                        return (
-                            <li>{e.}</li>
-                        )
-                    })
-                }
+            <div className="bg-slate-300 text-xl py-3 px-10 rounded-lg">
+                <h2>Received Data is:</h2>
+                <ul>
+                    {
+                        users.map((e) => {
+                            return (
+                                <li>{e.username}</li>
+                            )
+                        })
+                    }
+                </ul>
             </div>
         </div>
     )
